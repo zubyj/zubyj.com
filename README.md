bb59c238a8f51af335024f9573ae6908a7433c12
Zuby Javed <zubyjaved@gmail.com>
2023-03-09 20:57:32 -0800

    update readme

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
-### About
-Portfolio website showcasing my skills and projects.
+Check out the website [here](https://zubyjaved.com)
 
-### Technologies
+## Getting started
+Pretty standard node setup
+```
+npm i
+npm start
+```
+
+## About
+Portfolio website showcasing my skills and projects. 
+
+## Technologies
 ReactJS, Nginx, NodeJS, MaterialUI
+
+## Misc
+
+Feel free to copy the website and tailor it to your own projects & skills.
--- src/components/projects/ProjectsGreeting.js
+++ src/components/projects/ProjectsGreeting.js
@@ -7,25 +7,25 @@ function ProjectsGreeting() {
     const typewriter = () => {
         return (
             <Typewriter
-            onInit={(typewriter) => 
-                typewriter.typeString("<h2>Projects</h2>")
-                .changeDelay(50)
-                .typeString("<p><span class='Important-text'>(1)</span> iOS app in the App Store</p>")
-                .typeString("<p><span class='Important-text'>(2)</span> Websites live</p>")
-                .changeDelay(30)
-                .typeString("<p><span class='Important-text'>(2)</span> Web apps deployed</p>")
-                .typeString("<p><span class='Important-text'>(1)</span> Windows/MacOS apps available for download</p>")
-                .pauseFor(1000)
-                .typeString("<span class='App-link'>Stay tuned for more</span>")
-                .pauseFor(1000)
-                .start()
-            }
+                onInit={(typewriter) =>
+                    typewriter.typeString("<h2>Projects</h2>")
+                        .changeDelay(50)
+                        .typeString("<p><span class='Important-text'>(1)</span> iOS app in the App Store</p>")
+                        .typeString("<p><span class='Important-text'>(3)</span> Websites live</p>")
+                        .changeDelay(30)
+                        .typeString("<p><span class='Important-text'>(2)</span> Web apps deployed</p>")
+                        .typeString("<p><span class='Important-text'>(1)</span> Windows/MacOS apps available for download</p>")
+                        .pauseFor(1000)
+                        .typeString("<span class='App-link'>Stay tuned for more</span>")
+                        .pauseFor(1000)
+                        .start()
+                }
             />
         )
     }
 
     return (
-        <Greeting text={typewriter()} activePage="projects"/>
+        <Greeting text={typewriter()} activePage="projects" />
     )
 }
 
